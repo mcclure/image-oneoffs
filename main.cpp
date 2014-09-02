db79ce0de50b500115a8c332c5fff8b4abc778b9
mcc <andi.m.mcclure@gmail.com>
2014-09-01 23:41:56 -0700

    Fix memory access violation

--- main.cpp
+++ main.cpp
@@ -1,12 +1,25 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
 
 extern "C" {
 unsigned char *stbi_load(char const *filename, int *x, int *y, int *comp, int req_comp);
 extern int stbi_write_png(char const *filename, int w, int h, int comp, const void *data, int stride_in_bytes, int yflip);
 }
 
+// This program recreates a botched attempt to write a PNG exporter that flipped the Y axis as it encoded.
+// The attempt failed because the PNG encoder was using a form of compression where colors were stored
+// with values relative to their neighbors, but the flipping was confusing the notion of "neighbors".
+// To produce the effect, we take three steps:
+
+// 1. Pad the image with one row of black pixels, since the glitched encoder commits a memory access violation.
+#define SAFE 1
+
+// 2. Manually reverse the order of the pixel rows, so the flipped image looks right side up.
 #define PREFLIP 1
+
+// 3. Instruct the encoder to run its broken y-flip routine.
 #define YFLIP 1
 
 int main (int argc, const char * argv[]) {
@@ -22,6 +35,15 @@ int main (int argc, const char * argv[]) {
 		fprintf(stderr, "Could not open image %s\n", argv[1]);
 		return 1;
 	}
+
+#if SAFE
+	uint32_t *oldimage = image;
+	image = (uint32_t *)malloc(4*iwidth*(iheight+1));
+	memset(image, 0, 4*iwidth);
+	image += iwidth;
+	memcpy(image, oldimage, 4*iwidth*iheight);
+	free(oldimage);
+#endif
 
 #if PREFLIP
 	for(int y = 0; y < iheight/2; y++) {
